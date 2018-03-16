1aafb9b2289cc3d9f42eb22d7deaf93739128616
Javier López <javierlopez@uc.cl>
2018-03-16 14:10:56 -0700

    Change example model

--- app/helper.py
+++ app/helper.py
@@ -1,15 +1,26 @@
 from flask import jsonify, make_response
 from app import app
 
+
 def response(status, message, status_code):
     """
     Make an http response helper
     :param status: Status message
     :param message: Response Message
     :param status_code: Http response code
-    :return:
+    :return: Http response
     """
     return make_response(jsonify({
         'status': status,
         'message': message
     })), status_code
+
+def bad_request():
+    """
+    Make an http response helper for a bad request
+    :return: Http response
+    """
+    return make_response(jsonify({
+        'status': "failed",
+        'message': "Bad request, check your data payload"
+    })), 400
--- app/model/views.py
+++ app/model/views.py
@@ -1,8 +1,9 @@
-import os
-from flask import Blueprint, jsonify, request, render_template as view
-from app.helper import response
+import os, json
+from flask import Blueprint, jsonify, request
+from app.helper import response, bad_request
 import pandas as pd
 import dill as pickle
+import sklearn
 
 
 model = Blueprint('model', __name__)
@@ -10,51 +11,27 @@ model = Blueprint('model', __name__)
 @model.route('/predict', methods=['POST'])
 def predict():
 	"""
-	Receives input parameters to return a prediction by loading the previously trained model.
-	:return: Http Response
+	Receives input parameters to return a prediction 
+	by loading the previously trained model.
+	:return: Http response
 	"""
-	try:
-		test_json = request.get_json()
-		return response("OK", test_json, 200)
-		test = pd.read_json(test_json, orient='records')
-		print(test)
+	# Read request data
+	request_json = json.dumps(request.get_json())
+	test = pd.read_json(request_json, orient="records")
+	test = test[["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT"]]
 
+	# Load model
+	model_name = 'model_example.pk'
+	with open('./app/model/models/' + model_name, 'rb') as file:
+		model = pickle.load(file)
 
-		#To resolve the issue of TypeError: Cannot compare types 'ndarray(dtype=int64)' and 'str'
-		test['Dependents'] = [str(x) for x in list(test['Dependents'])]
+	# Make predictions
+	predictions = model.predict(test)
 
-		#Getting the Loan_IDs separated out
-		loan_ids = test['Loan_ID']
-
-	except Exception as e:
-		raise e
-
-	clf = 'model_v1.pk'
+	# Arrange response data
+	final_predictions = [{"price": p[0]} for p in predictions]
 	
-	if test.empty:
-		return(bad_request())
-	else:
-		#Load the saved model
-		print("Loading the model...")
-		loaded_model = None
-		with open('./model/models/' + clf,'rb') as f:
-			loaded_model = pickle.load(f)
+	responses = jsonify(predictions=final_predictions)
+	responses.status_code = 200
 
-		print("The model has been loaded...doing predictions now...")
-		predictions = loaded_model.predict(test)
-		
-		"""Add the predictions as Series to a new pandas dataframe
-								OR
-		   Depending on the use-case, the entire test data appended with the new files
-		"""
-		prediction_series = list(pd.Series(predictions))
-
-		final_predictions = pd.DataFrame(list(zip(loan_ids, prediction_series)))
-		
-		"""We can be as creative in sending the responses.
-		   But we need to send the response codes as well.
-		"""
-		responses = jsonify(predictions=final_predictions.to_json(orient="records"))
-		responses.status_code = 200
-
-		return (responses)
+	return responses
