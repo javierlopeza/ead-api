a8f22dd0a7eb98f758007a535932fbb7461fc81c
Javier López <javierlopez@uc.cl>
2018-04-25 11:51:49 -0700

    Add model file

--- app/model/views.py
+++ app/model/views.py
@@ -9,7 +9,7 @@ import sklearn
 model = Blueprint('model', __name__)
 
 @model.route('/predict', methods=['POST'])
-def predict():
+def predict_example():
 	"""
 	Receives input parameters to return a prediction 
 	by loading the previously trained model.
@@ -35,6 +35,29 @@ def predict():
 	responses.status_code = 200
 
 	return responses
+
+@model.route('/predict_ead', methods=['POST'])
+def predict_ead():
+	# Read request data to pandas dataframe
+	request_json = json.dumps(request.get_json())
+	data = pd.read_json(request_json)
+	columns = ['age_months', 'anxiety', 'hand_finger_mannerisms', 'imagination_creativity', 'immediate_echolalia', 'quality_social_overtures', 'self_injurious_behavior', 'shared_enjoyment_interaction', 'tantrums_aggression_disruptive_behavior', 'unusual_eye_contact', 'is_male']
+	data = data[columns]
+
+	# Load model 
+	model_name = 'model_v1.pk'
+	with open('./app/model/models/' + model_name, 'rb') as file:
+		model = pickle.load(file)
+
+	# Make predictions
+	predictions = model.predict(data)
+
+	# Arrange response data
+	final_prediction = int(predictions[0])
+	response = jsonify(has_asd=final_prediction)
+	response.status_code = 200
+
+	return response
 
 
 from random import randint
