3f014bdeae528e41626f556f1f772124e682450c
Javier López <javierlopez@uc.cl>
2018-04-25 11:56:48 -0700

    Refactor prediction endpoints

--- app/model/views.py
+++ app/model/views.py
@@ -8,34 +8,6 @@ import sklearn
 
 model = Blueprint('model', __name__)
 
-@model.route('/predict', methods=['POST'])
-def predict_example():
-	"""
-	Receives input parameters to return a prediction 
-	by loading the previously trained model.
-	:return: Http response
-	"""
-	# Read request data
-	request_json = json.dumps(request.get_json())
-	test = pd.read_json(request_json, orient="records")
-	test = test[["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT"]]
-
-	# Load model
-	model_name = 'model_example.pk'
-	with open('./app/model/models/' + model_name, 'rb') as file:
-		model = pickle.load(file)
-
-	# Make predictions
-	predictions = model.predict(test)
-
-	# Arrange response data
-	final_predictions = [{"price": p[0]} for p in predictions]
-	
-	responses = jsonify(predictions=final_predictions)
-	responses.status_code = 200
-
-	return responses
-
 @model.route('/predict_ead', methods=['POST'])
 def predict_ead():
 	# Read request data to pandas dataframe
@@ -57,12 +29,4 @@ def predict_ead():
 	response = jsonify(has_asd=final_prediction)
 	response.status_code = 200
 
-	return response
-
-
-from random import randint
-@model.route('/predict2', methods=['POST'])
-def predict2():
-	response = jsonify(results=[randint(0,10) for _ in range(10)])
-	response.status_code = 200
 	return response
